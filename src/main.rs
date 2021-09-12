8b8167a66c41db13c830a566a77e37a16ee76a5c
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-09-12 20:01:30 +0200

    path to tempdir is known

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ async fn main() -> Result<()> {
     let station_id_osna = "01766";
     let tmp_dir = Builder::new().prefix("historical_weather").tempdir()?;
     let zipfile = download_zip_archive(tmp_dir.path(), station_id_osna).await?;
-    let zipdir = zipfile.parent().expect("Tempdir did not work");
+    let zipdir = tmp_dir.path();
     let file = fs::File::open(&zipfile)?;
     let mut archive = zip::ZipArchive::new(file).unwrap();
     println!("DONE");
