cd430ac6866f1b74b20fd0093987d6611b3ec354
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-09-12 19:57:56 +0200

    fix clippy warning

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn download_zip_archive(download_path: &Path, station_id: &str) -> Result<
 async fn main() -> Result<()> {
     let station_id_osna = "01766";
     let tmp_dir = Builder::new().prefix("historical_weather").tempdir()?;
-    let zipfile = download_zip_archive(&tmp_dir.path(), station_id_osna).await?;
+    let zipfile = download_zip_archive(tmp_dir.path(), station_id_osna).await?;
     let zipdir = zipfile.parent().expect("Tempdir did not work");
     let file = fs::File::open(&zipfile)?;
     let mut archive = zip::ZipArchive::new(file).unwrap();
