dd9a2de9b599b8e627969c851b53c616739d3df8
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-09-15 13:31:47 +0200

    tests

--- src/data_access.rs
+++ src/data_access.rs
@@ -27,35 +27,56 @@ pub fn load_data(path: &Path) -> Result<Vec<TemperatureMeasurement>> {
     for result in rdr.records() {
         let record = result?;
         let date_string = record.get(1).unwrap().to_string();
-        let year = date_string
-            .chars()
-            .take(4)
-            .collect::<String>()
-            .parse::<i32>()?;
-        let month = date_string
-            .chars()
-            .skip(4)
-            .take(2)
-            .collect::<String>()
-            .parse::<u32>()?;
-        let day = date_string
-            .chars()
-            .skip(6)
-            .take(2)
-            .collect::<String>()
-            .parse::<u32>()?;
-        let hour = date_string
-            .chars()
-            .skip(8)
-            .take(2)
-            .collect::<String>()
-            .parse::<u32>()?;
+        let datetime = parse_date_string(&date_string)?;
 
         measurement_vec.push(TemperatureMeasurement::new(
-            NaiveDate::from_ymd(year, month, day).and_hms(hour, 0, 0),
+            datetime,
             record.get(3).unwrap().trim().parse::<f32>()?,
         ));
     }
 
     Ok(measurement_vec)
+}
+
+fn parse_date_string(datetime_string: &str) -> Result<NaiveDateTime> {
+    let year = datetime_string
+        .chars()
+        .take(4)
+        .collect::<String>()
+        .parse::<i32>()?;
+    let month = datetime_string
+        .chars()
+        .skip(4)
+        .take(2)
+        .collect::<String>()
+        .parse::<u32>()?;
+    let day = datetime_string
+        .chars()
+        .skip(6)
+        .take(2)
+        .collect::<String>()
+        .parse::<u32>()?;
+    let hour = datetime_string
+        .chars()
+        .skip(8)
+        .take(2)
+        .collect::<String>()
+        .parse::<u32>()?;
+    Ok(NaiveDate::from_ymd(year, month, day).and_hms(hour, 0, 0))
+}
+
+#[cfg(test)]
+mod data_access_tests {
+    use chrono::NaiveTime;
+
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_correct_string() {
+        let fake_date = "2020010110".to_owned();
+        let res = parse_date_string(&fake_date).unwrap();
+        assert_eq!(res.date(), NaiveDate::from_ymd(2020, 1, 1));
+        assert_eq!(res.time(), NaiveTime::from_hms(10, 0, 0));
+    }
 }
